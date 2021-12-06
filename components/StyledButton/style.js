ccf7286ac9c0434731d642747f14e9cb0e64aae2
rishi1606 <85154647+rishi1606@users.noreply.github.com>
2021-12-06 13:08:47 +0530

    Add files via upload

---
+++ components/StyledButton/style.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+const styles=StyleSheet.create({
+    Container:{
+        width:"100%",
+        padding:10
+    },
+    button:{
+        
+        height:40,
+        justifyContent:"center",
+        alignItems:"center",
+        borderRadius:20,
+        
+    },
+    Text:{
+        fontSize:14,
+        fontWeight:"700",
+        textTransform:"uppercase",
+        
+    }
+})
+export default styles;
